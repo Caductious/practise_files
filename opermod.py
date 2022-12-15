0201e5ccc99deaf20f47c1bf8d87d105c5705b27
Caduct <112777467+Caductious@users.noreply.github.com>
2022-12-15 20:26:55 +0300

    Add files via upload

--- opermod.py
+++ opermod.py
@@ -23,7 +23,7 @@ def save_table(file_name,dict1):
         string+=''.join(dict1['values'][y][x])+','
     with open(file_name, 'wb') as f:
       pickle.dump(string[:-1], f)
-#Хуёво работает
+
 def get_column_types(dict2):
   booleanExcel=['ИСТИНА','1','ЛОЖЬ','0']
   by_number=input('Столбец будет искаться по индексу? ДА/НЕТ').upper()
@@ -38,7 +38,7 @@ def get_column_types(dict2):
     name=input('введите заголовок столбца')
     assert name in dict2['names'], 'Такого столбца нет'
     number=dict2['names'].index(name)
-  if dict2['values'][number][0] in booleanExcel:
+  if dict2['values'][number][1] in booleanExcel:
     typp=bool
   else:
     try:
@@ -105,10 +105,12 @@ def get_rows_by_index(dict1gri):
   else:
     save_table(input('введите название изначального файла с указанием формата'), newdictgri)
   return(newdictgri)
-#не работает
+
 def get_values(dict1):
   lst=[]
   column=input('введите название или номер колонки')
+  if column=='':
+    column=1
   try:
     column=int(column)-1
   except ValueError:
@@ -116,16 +118,17 @@ def get_values(dict1):
     column=dict1['names'].index(column)
   lst=[name for name in (dict1['values'][column])]
   return(lst)
-#работает
+
 def get_value(dict3):
-  column=0
   column=input('введите название или номер колонки')
+  if column=='':
+    column=1
   try:
     column=int(column)-1
   except ValueError:
     assert column in dict3['names'],'такой колонки нет'
     column=dict3['names'].index(column)
-  return(dict3['values'][column])
+  return(dict3['values'][column][0])
 
 def print_table(dict4):
   out1=dict4['names']
@@ -184,3 +187,44 @@ def save_table(file_name,dict1):
         textfile.write('-'*len(out2)+'\n')
     textfile.close()
 
+def set_column_types(dict5):
+  by_number = input('Столбец будет изменяться по индексу? ДА/НЕТ').upper()
+  assert by_number == 'ДА' or by_number == 'НЕТ', 'введено неверное значение'
+  if by_number == 'ДА':
+    by_number = True
+  elif by_number == 'НЕТ':
+    by_number = False
+  if by_number:
+    number = int(input('введите номер столбца')) - 1
+  else:
+    name = input('введите заголовок столбца')
+    assert name in dict5['names'], 'Такого столбца нет'
+    number = dict5['names'].index(name)
+  typ=input('Введите требуемый тип значений')
+  typp={name:typ}
+  return typp
+
+def set_values(values,dict6):
+  lst = []
+  column=input('введите название или номер колонки')
+  if column=='':
+    column=1
+  try:
+    column = int(column) - 1
+  except ValueError:
+    assert column in dict6['names'], 'такой колонки нет'
+    column = dict6['names'].index(column)
+  dict6['values'][column]=[name for name in (values)]
+  return (dict6['values'][column])
+
+def set_value(value,dict7):
+  column=input('введите название или номер колонки')
+  if column == '':
+    column = 1
+  try:
+    column=int(column)-1
+  except ValueError:
+    assert column in dict7['names'],'такой колонки нет'
+    column=dict7['names'].index(column)
+  dict7['values'][column][0]=value
+  return(dict7['values'][column][0])
